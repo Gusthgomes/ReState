fd9363899524a88fd14bde45bf38076412f1da05
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2024-12-31 15:44:43 -0300

    feat: create Sign-In UI

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -6,7 +6,7 @@ export default function index() {
   return (
     <View className="flex justify-center items-center w-full h-screen" >
 
-      <Text className='text-red-600 font-bold'>welcome to ReState</Text>
+      <Text className='font-bold my-10 font-rubik text-3xl'>Welcome to ReState</Text>
       
       <Link href="/sign-in" className='text-red-500'>Sign-In</Link>
       <Link href="/(root)/(tabs)/explore" className='text-green-500'>Explore</Link>
--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,11 +1,39 @@
-import { View, Text } from 'react-native'
+import { SafeAreaView, ScrollView, Image, View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
+import images from '@/constants/images'
+import icons from '@/constants/icons'
 
 const SignIn = () => {
+
+  const handleLogin = () => {};
+
   return (
-    <View>
-      <Text>sign-in</Text>
-    </View>
+    <SafeAreaView className='bg-white h-full mt-5'>
+      <ScrollView contentContainerClassName='h-screen'>
+        <Image source={ images.onboarding} className='w-full h-4/6' resizeMode='cover'/>
+
+        <View className='px-10'>
+          <Text className='text-base text-center uppercase font-rubik text-black-200'>Bem vindo(a) ao ReState!</Text>
+
+          <Text className="text-2xl font-rubik-bold text-black-300 text-center mt-2">
+            vamos chegar mais perto da {"\n"}
+            
+            <Text className='text-primary-300'>
+                Sua casa ideal!
+            </Text>
+          </Text>
+
+          <Text className='text-lg font-rubik text-black-200 text-center mt-3'>Faça login com o Google</Text>
+
+          <TouchableOpacity onPress={ handleLogin } className='bg-white shadow-md shadow-zinc-500 rounded-full w-full py-2 mt-2'>
+            <View className='flex flex-row justify-center items-center'>
+              <Image source={icons.google} className='w-5 h-5' resizeMode='contain'/>
+              <Text className='text-lg text-black-300 font-rubik ml-3'>Continue com o Google</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
