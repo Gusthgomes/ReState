f6c7a7d544932f6bd78750a366a490e51569eec1
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2024-12-31 15:44:07 -0300

    feat: deactiving header

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout () {
   }
 
   return (
-    <Stack>
+    <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="/(root)/(tabs)/index" options={{ headerShown: false }}/>
     </Stack>
   )
